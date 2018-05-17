176320b7771ed764eebe1993de50752e7bc99adf
Chris Whinfrey <cwhinfrey@gmail.com>
2018-05-17 01:49:49 -0400

    Stub out contracts

--- future-apps/futarchy/contracts/Futarchy.sol
+++ future-apps/futarchy/contracts/Futarchy.sol
@@ -1,9 +1,33 @@
-pragma solidity 0.4.18;
+pragma solidity 0.4.23;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
 
 import "@aragon/os/contracts/common/IForwarder.sol";
 
-contract Futarchy is iForwarder, AragonApp {
+contract Futarchy is IForwarder, AragonApp {
+  
+    function initialize() onlyInit external {
+        initialized();
+    }
 
+    function newFutarchyMarket(bytes _executionScript, string _metadata) external returns (uint256 marketId) {
+      
+    }
+
+    function isForwarder() public pure returns (bool) {
+        return true;
+    }
+
+    function forward(bytes _evmScript) public {
+        require(canForward(msg.sender, _evmScript));
+    }
+
+    function canForward(address _sender, bytes _evmCallScript) public view returns (bool) {
+      // TODO: Check if sender can forward
+      return true;
+    }
+
+    function executeDecision(uint256 _marketId) internal {
+        
+    }
 }
